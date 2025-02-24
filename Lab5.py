c62cf1cbedafe29c39d2b1f2b55b6676b1767c54
Danny <95605537+Sheepeyy@users.noreply.github.com>
2025-02-24 14:52:15 +0000

    Halfway done

--- Lab5.py
+++ Lab5.py
@@ -5,13 +5,35 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+        # pattern = [1 , 0 , 0 , 0 , 1 ]
+    result = ""
+    
+    count = [1/n , 0/n , 0/n , 0/n , 1/n]
+    while count == 1:
+        for i in range(n,n + 1):
+            for j in range(n,n + 1):
+                result += "*"
+            print("*****", end=" ")
+        result += "\n"
+    while count == 0:
+        for i in range(n,n + 1):
+            for j in range(n,n + 1):
+                result += "*   *"
+            print("*   *", end=" ")
+        result += "\n"
+    return result
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
+    result: ""
+    count = 1
+    while count <= 1:
+        print(count)
+        count += 1
+        result += "\n"
     return ""
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
